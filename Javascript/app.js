c02cc22694f6ecf6d3490242a8fa26074e55cc63
Eze Chiemerie <ezehdavid960@outlook.com>
2022-08-16 15:26:03 +0100

    Commit

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,18 +1,18 @@
-const navigation = document.querySelector('.navlinks');
-const btn = document.querySelector('.hamburger');
-const modal = document.querySelector('.modal')
-const body = document.querySelector('.body')
-const close = document.querySelector('.close')
-const doc = document.querySelector('body')
-btn.addEventListener('click', () => {
-  navigation.classList.toggle('active')
-})
-modal.addEventListener('click', (e) => {
-  e.preventDefault()
-  body.classList.add('blur')
-  doc.setAttribute('style',  "overflow: hidden;")
-})
-close.addEventListener('click', (e) => {
-  body.classList.remove('blur')
-  doc.setAttribute('style',  "overflow: auto;")
-})
+const navigation = document.querySelector(".navlinks");
+const btn = document.querySelector(".hamburger");
+const modal = document.querySelector(".modal");
+const body = document.querySelector(".body");
+const close = document.querySelector(".close");
+const doc = document.querySelector("body");
+btn.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+});
+modal.addEventListener("click", (e) => {
+  e.preventDefault();
+  body.classList.add("blur");
+  doc.setAttribute("style", "overflow: hidden;");
+});
+close.addEventListener("click", (e) => {
+  body.classList.remove("blur");
+  doc.setAttribute("style", "overflow: auto;");
+});
