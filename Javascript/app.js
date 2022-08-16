f6105f1414989aa4758cde5921e26d419e5a3967
Eze Chiemerie <ezehdavid960@outlook.com>
2022-08-16 11:05:55 +0100

    commit

--- Javascript/app.js
+++ Javascript/app.js
@@ -2,10 +2,14 @@ const navigation = document.querySelector('.navlinks');
 const btn = document.querySelector('.hamburger');
 const modal = document.querySelector('.modal')
 const body = document.querySelector('.body')
+const close = document.querySelector('.close')
 btn.addEventListener('click', () => {
   navigation.classList.toggle('active')
 })
 modal.addEventListener('click', (e) => {
   e.preventDefault()
   body.classList.add('blur')
+})
+close.addEventListener('click', (e) => {
+  body.classList.remove('blur')
 })
